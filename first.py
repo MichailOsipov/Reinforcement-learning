ecabb8eab0fd9cc8e3072433fd011485f7f811cc
Michail <osimike96@gmail.com>
2018-05-10 01:34:12 +0300

    Added Gibbs algorithm. Greedy and Gibbs comparision.

--- first.py
+++ first.py
@@ -1,29 +1,18 @@
 import numpy as np
+import math
 import matplotlib.pyplot as plt
 
-minExpected = 1
-maxExpected = 100
-dispersion = 1
-automatsCount = 20
-def randomNumberInRange(x):
-    return np.random.randint(minExpected, maxExpected)
+def randomWithDispersion(dispersion):
+    def randomWithExpectedValue(expectedValue):
+        def fixedDispersionAndExpected(arg = None):
+            return np.random.normal(expectedValue, dispersion)
+        return fixedDispersionAndExpected
+    return randomWithExpectedValue
 
-def randomWithExpectedValue(expectedValue):
-    def fixedExpected():
-        return np.random.normal(expectedValue, dispersion)
-    return fixedExpected
-
-generatedExpectedValues = np.vectorize(randomNumberInRange)
-generateBandits = np.vectorize(randomWithExpectedValue)
-expectedValuesToBandits = generatedExpectedValues([0] * automatsCount)
-print("expected:")
-print(expectedValuesToBandits)
-bandits = generateBandits(expectedValuesToBandits)
-    
 def startSimulationGreedy(bandits, iterationsCount, greedyConstant):
     rewardsSumm = [0] * len(bandits)
     selectionsCount = [0] * len(bandits)
-    valuation = [0] * len(bandits)
+    valuations = [0] * len(bandits)
     allValuesSum = 0
     averageReward = []
     
@@ -35,27 +24,123 @@ def startSimulationGreedy(bandits, iterationsCount, greedyConstant):
         banditValue = bandits[banditIndex]()
         rewardsSumm[banditIndex] = rewardsSumm[banditIndex] + banditValue
         selectionsCount[banditIndex] = selectionsCount[banditIndex] + 1
-        valuation[banditIndex] = rewardsSumm[banditIndex] / selectionsCount[banditIndex]
+        valuations[banditIndex] = rewardsSumm[banditIndex] / selectionsCount[banditIndex]
         return banditValue
         
     for i in range(0, iterationsCount):
         isGreedy = returnTrueWithProbability(1 - greedyConstant)
         indexOfSelected = 0
         if isGreedy:
-            indexOfSelected = valuation.index(max(valuation))
+            indexOfSelected = valuations.index(max(valuations))
         else:
             indexOfSelected = np.random.randint(0, len(bandits))
         selectedValue = selectBandit(indexOfSelected)
         allValuesSum = allValuesSum + selectedValue
         averageReward.append(allValuesSum / (i + 1))
-    return valuation, averageReward
-      
-greedyConstants = [0, 0.01, 0.1]
+    return valuations, averageReward
 
+def startSimulationGibbs(bandits, iterationsCount, temperature):
+    rewardsSumm = [0] * len(bandits)
+    selectionsCount = [0] * len(bandits)
+    valuations = [0] * len(bandits)
+    allValuesSum = 0
+    averageReward = []
+    
+    def calcPossibilities():
+        possibilities = [0] * len(valuations)
+        valuationSum = 0
+        for valuation in valuations:
+            valuationSum = valuationSum + math.exp(valuation / temperature)
+        for j in range(0, len(possibilities)):
+            possibilities[j] = math.exp(valuations[j] / temperature) / valuationSum
+        return possibilities
+    def chooseRandomByPossibilities(possibilities):
+        randomNumber = np.random.uniform(0, 1)
+        possibilitiesSum = 0
+        for i in range(0, len(possibilities)):
+            possibilitiesSum = possibilitiesSum + possibilities[i]
+            if randomNumber < possibilitiesSum:
+                return i
+        return 0
+    
+    def selectBandit(banditIndex):
+        banditValue = bandits[banditIndex]()
+        rewardsSumm[banditIndex] = rewardsSumm[banditIndex] + banditValue
+        selectionsCount[banditIndex] = selectionsCount[banditIndex] + 1
+        valuations[banditIndex] = rewardsSumm[banditIndex] / selectionsCount[banditIndex]
+        return banditValue
+    
+    for i in range(0, iterationsCount):
+        possibilities = calcPossibilities()
+        indexOfSelected = chooseRandomByPossibilities(possibilities)
+        selectedValue = selectBandit(indexOfSelected)
+        allValuesSum = allValuesSum + selectedValue
+        averageReward.append(allValuesSum / (i + 1))
+    return valuations, averageReward
+    
+    
+def addAverageRewardsToPlot(averageReward, label):
+    plt.plot(np.arange(len(averageReward)), averageReward, label = label)
+
+banditsCount = 10
 iterationsCount = 1000
-for greedyConstant in greedyConstants:
-    valuation, averageReward = startSimulationGreedy(bandits, iterationsCount, greedyConstant)
-    plt.plot(np.arange(len(averageReward)), averageReward, label = greedyConstant)
+tasksCount = 2000
 
+dispersion = 1
+
+generateExpectedValues = np.vectorize(randomWithDispersion(1)(0))
+generateBandits = np.vectorize(randomWithDispersion(dispersion))
+
+def startGreedyAlgTest():
+    greedyConstants = [0, 0.01, 0.1]
+    for greedyConstant in greedyConstants:
+        allAverageRewards = np.zeros(iterationsCount)
+        for i in range(0, tasksCount):
+            expectedValues = generateExpectedValues(np.zeros(banditsCount))
+            bandits = generateBandits(expectedValues)
+            valuations, averageReward = startSimulationGreedy(bandits, iterationsCount, greedyConstant)
+            allAverageRewards = allAverageRewards + averageReward
+            if i % 100 == 0:
+                print(i)
+        allAverageRewards = allAverageRewards / tasksCount
+        addAverageRewardsToPlot(allAverageRewards, 'greedy: ' + str(greedyConstant))
+
+def startGibbsAlgTest():
+    temperatures = [0.1, 0.5, 1, 10]
+    for temperature in temperatures:
+        allAverageRewards = np.zeros(iterationsCount)
+        for i in range(0, tasksCount):
+            expectedValues = generateExpectedValues(np.zeros(banditsCount))
+            bandits = generateBandits(expectedValues)
+            valuations, averageReward = startSimulationGibbs(bandits, iterationsCount, temperature)
+            allAverageRewards = allAverageRewards + averageReward
+            if i % 100 == 0:
+                print(i)
+        allAverageRewards = allAverageRewards / tasksCount
+        addAverageRewardsToPlot(allAverageRewards, 'gibbs: ' + str(temperature))
+
+def startDispercionGreedyComparision():
+    dispersions = [1, 2, 5]
+    expectedValues = generateExpectedValues(np.zeros(banditsCount))
+    for disp in dispersions:
+        generateCustomBandits = np.vectorize(randomWithDispersion(disp))
+        bandits = generateCustomBandits(expectedValues)
+        valuations, averageReward = startSimulationGreedy(bandits, iterationsCount, 0.1)
+        addAverageRewardsToPlot(averageReward, 'disp: ' + str(disp))
+
+def startDispercionGibbsComparision():
+    dispersions = [1, 2, 5]
+    expectedValues = generateExpectedValues(np.zeros(banditsCount))
+    for disp in dispersions:
+        generateCustomBandits = np.vectorize(randomWithDispersion(disp))
+        bandits = generateCustomBandits(expectedValues)
+        valuations, averageReward = startSimulationGibbs(bandits, iterationsCount, 0.1)
+        addAverageRewardsToPlot(averageReward, 'disp: ' + str(disp))
+
+#startGreedyAlgTest()
+#startGibbsAlgTest()
+#startDispercionGreedyComparision()   
+#startDispercionGibbsComparision()
 plt.legend()
 plt.show()
+    
